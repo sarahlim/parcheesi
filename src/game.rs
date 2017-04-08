b1f8427aedc4f59d46c630ff60faf71a8cff80b6
Sarah Lim <sarah@sarahlim.com>
2017-04-08 16:29:13 -0500

    Added Dice types and tests, Move types

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+
+use self::rand::Rng;
+
 #[derive(Debug, Clone, PartialEq)]
 /// Represents a color of a piece or player.
 pub enum Color {
@@ -7,15 +11,15 @@ pub enum Color {
     Yellow,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 /// Represents a pawn on the board.
 pub struct Pawn {
-    id: i32, // 0..3
+    id: usize, // 0..3
     color: Color,
 }
 
 impl Pawn {
-    pub fn new(id: i32, color: Color) -> Pawn {
+    pub fn new(id: usize, color: Color) -> Pawn {
         Pawn {
             id: id,
             color: color,
@@ -23,6 +27,42 @@ impl Pawn {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+/// Represents a move selected by the player.
+pub enum Move {
+    MoveMain {
+        pawn: Pawn,
+        start: usize,
+        distance: usize,
+    },
+    MoveHome {
+        pawn: Pawn,
+        start: usize,
+        distance: usize,
+    },
+    EnterPiece { pawn: Pawn },
+}
+
+#[derive(Debug, Clone)]
+/// Holds the result of two die rolls.
+struct Dice(usize, usize);
+
+/// Simulates the result of rolling two dice.
+fn roll_dice() -> Dice {
+    let d1 = rand::thread_rng().gen_range(1, 7);
+    let d2 = rand::thread_rng().gen_range(1, 7);
+
+    Dice(d1, d2)
+}
+
+// #[derive(Debug, Clone)]
+/// Generic Player trait provides an interface for the
+/// server to interact with players.
+// trait Player {
+//     fn start_game(&self, color: Color) -> ();
+//     fn do_more(&self, board: Board, dice: Dice) -> Move;
+//     fn doubles_penalty(&self) -> ();
+// }
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +74,28 @@ mod tests {
         let r2 = Pawn::new(2, Color::Red);
         assert_ne!(y1.color, r2.color);
         assert_eq!(r1.color, r2.color);
+    }
+
+    #[test]
+    /// Make sure dice are fair.
+    fn check_dice() {
+        let mut freq = [0; 13]; // 2...12 are the outcomes
+        let iters = 10000;
+
+        for i in 0..iters {
+            let Dice(d1, d2) = roll_dice();
+            let sum = d1 + d2;
+            freq[sum] += 1;
+        }
+
+        let mut max_index = 0;
+
+        for (j, &value) in freq.iter().enumerate() {
+            if value > freq[max_index] {
+                max_index = j;
+            }
+        }
+
+        assert_eq!(max_index, 7);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ mod game;
 
 fn main() {
     println!("Hello, world!");
+    let p = game::Pawn::new(1, game::Color::Yellow);
+    let m1 = game::Move::EnterPiece { pawn: p };
+    // println!("{}!", p.id);
 }
