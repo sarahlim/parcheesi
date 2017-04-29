404558275d5ca6c2f6d8d0f9d2981d513ef7ee39
JAlberss <johnalbers2018@u.northwestern.edu>
2017-04-29 12:44:42 -0500

    Actually added the players

--- src/player.rs
+++ src/player.rs
@@ -1,22 +1,23 @@
 use super::board::{Color, Board};
-use super::game::Move;
+use super::game::{Move};
 use super::dice::Dice;
-
-
-
-
 
 /// Generic Player trait provides an interface for the
 /// server to interact with players.
 pub trait Player {
     /// Inform the Player that a game has started, and
     /// what color the player is.
-    fn start_game(&self, color: Color) -> ();
+    fn start_game(&self, color: Color) -> () {
+        println!("The game is start {:?}",color)
+    }
+        
 
     /// Ask the player what move they want to make.
     fn do_move(&self, board: Board, dice: Dice) -> Vec<Move>;
 
     /// Inform the player that they have suffered a doubles
     /// penalty.
-    fn doubles_penalty(&self) -> ();
+    fn doubles_penalty(&self) -> () {
+        println!("Penalty on me")
+    }
 }
